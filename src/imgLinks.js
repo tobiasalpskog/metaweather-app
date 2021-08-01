c53c13d00f82b92184d313d0904be89b680b354a
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-01 15:11:42 +0200

    image links for metaweather svgs

---
+++ src/imgLinks.js
@@ -0,0 +1,14 @@
+const imgLinks = {
+	sn: "https://www.metaweather.com/static/img/weather/sn.svg",
+	sl: "https://www.metaweather.com/static/img/weather/sl.svg",
+	h: "https://www.metaweather.com/static/img/weather/h.svg",
+	t: "https://www.metaweather.com/static/img/weather/t.svg",
+	hr: "https://www.metaweather.com/static/img/weather/hr.svg",
+	lr: "https://www.metaweather.com/static/img/weather/lr.svg",
+	s: "https://www.metaweather.com/static/img/weather/s.svg",
+	hc: "https://www.metaweather.com/static/img/weather/hc.svg",
+	lc: "https://www.metaweather.com/static/img/weather/lc.svg",
+	c: "https://www.metaweather.com/static/img/weather/c.svg",
+};
+
+export default imgLinks;
