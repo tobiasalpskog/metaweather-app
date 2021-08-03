c49e23cd83acf15a26b18715a6f7a37476391759
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-03 11:00:30 +0200

    added utility class align-center

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -3,7 +3,7 @@ import Find from "./Find";
 
 export default function Location({ updateLocation }) {
 	return (
-		<div className="flex row space-between mvfull p-4">
+		<div className="flex row space-between align-center mvfull p-4">
 			<Search />
 			<Find updateLocation={updateLocation} />
 		</div>
