16c32395c61c116cc6e5e85a344cc13e68915e3a
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-03 12:05:33 +0200

    added updateLocation prop to Search component

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -4,7 +4,7 @@ import Find from "./Find";
 export default function Location({ updateLocation }) {
 	return (
 		<div className="flex row space-between align-center mvfull p-4">
-			<Search />
+			<Search updateLocation={updateLocation} />
 			<Find updateLocation={updateLocation} />
 		</div>
 	);
