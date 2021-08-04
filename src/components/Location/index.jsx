3e0d41274eec8312f60e43e256f9001d18b64c5d
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-04 16:17:01 +0200

    moved lg-fixed vfull from div to header to fix width issue

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -3,7 +3,7 @@ import Find from "./Find";
 
 export default function Location({ updateLocation }) {
 	return (
-		<div className="flex row space-between align-center mvfull p-4 lg-fixed vfull">
+		<div className="flex row space-between align-center mvfull p-4">
 			<Search updateLocation={updateLocation} />
 			<Find updateLocation={updateLocation} />
 		</div>
