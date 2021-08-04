fb4b115624c10b0dd7f850e34574e1d82210badd
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-04 14:11:27 +0200

    added exit animations and fadeIns of data

--- src/components/Weather/Main.jsx
+++ src/components/Weather/Main.jsx
@@ -2,7 +2,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import imgLinks from "../../imgLinks";
 
-import { fadeIn, container, item } from "../../variants";
+import { fadeIn, fadeOut, list, item } from "../../variants";
 
 import {
 	getMonthFromDate,
@@ -21,40 +21,73 @@ export default function Main({
 
 	return (
 		<section className="main p-8 pt-0 flex column space-between">
-			<div>
+			<motion.div variants={fadeIn} initial="hidden" animate="animate">
 				<p className="material-icons md-36 vfull red text centered mb-0">
 					room
 				</p>
 				<h1 className="text centered mtb-0" id="selected-city">
 					{city}
 				</h1>
-			</div>
-			{data.the_temp !== undefined && (
-				<>
-					<div className="flex column center">
-						<img
+			</motion.div>
+			<AnimatePresence exitBeforeEnter initial={false}>
+				{data.the_temp && (
+					<>
+						<motion.img
 							src={imgLinks[data.weather_state_abbr]}
 							alt={alt}
 							className="centered v40 self-start"
+							variants={fadeIn}
+							initial="hidden"
+							animate="animate"
+							exit={fadeOut}
+							key={`anim-img-${city}`}
 						/>
-						<p className="text centered p-2 huge mt-0 mb-0">{`${data.the_temp} °C`}</p>
-						<span className="text centered p-2">{data.weather_state_name}</span>
-					</div>
-					<span className="text centered p-2">
-						{`Today — ${new Date().toDateString().slice(0, -5)}`}
-					</span>
-					<button
-						className={
-							isShowingDetails
-								? "centered toggle transition smooth-6 color bg collapsed"
-								: "centered toggle transition smooth-6 color bg expanded"
-						}
-						onClick={() => toggleExpandedView()}
-					>
-						Show Details
-					</button>
-				</>
-			)}
+						<motion.p
+							className="text centered p-2 huge mt-0 mb-0"
+							variants={fadeIn}
+							initial="hidden"
+							animate="animate"
+							exit={fadeOut}
+							key={`anim-temp-${city}`}
+						>{`${data.the_temp} °C`}</motion.p>
+						<motion.span
+							className="text centered p-2"
+							variants={fadeIn}
+							initial="hidden"
+							animate="animate"
+							exit={fadeOut}
+							key={`anim-state-${city}`}
+						>
+							{data.weather_state_name}
+						</motion.span>
+						<motion.span
+							className="text centered p-2"
+							variants={fadeIn}
+							initial="hidden"
+							animate="animate"
+							exit={fadeOut}
+							key={`anim-date-${city}`}
+						>
+							{`Today — ${new Date().toDateString().slice(0, -5)}`}
+						</motion.span>
+						<motion.button
+							className={
+								isShowingDetails
+									? "centered toggle transition smooth-6 color bg collapsed"
+									: "centered toggle transition smooth-6 color bg expanded"
+							}
+							onClick={() => toggleExpandedView()}
+							variants={fadeIn}
+							initial="hidden"
+							animate="animate"
+							exit={fadeOut}
+							key={`anim-toggleDetails-${city}`}
+						>
+							Show Details
+						</motion.button>
+					</>
+				)}
+			</AnimatePresence>
 		</section>
 	);
 }
