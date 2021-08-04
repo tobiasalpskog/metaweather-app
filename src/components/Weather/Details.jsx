80d367d1c75326c887b9008aa5aebe637f13a875
Tobias Alpskog <alpskogtobias@gmail.com>
2021-08-04 14:09:30 +0200

    added wind direction control flow and icons

--- src/components/Weather/Details.jsx
+++ src/components/Weather/Details.jsx
@@ -24,6 +24,35 @@ export default function Details({
 		}
 	}, [isShowingDetails]);
 
+	let windDirection;
+
+	switch (data.wind_direction_compass) {
+		case "N":
+			windDirection = "north";
+			break;
+		case "NE":
+			windDirection = "north_east";
+			break;
+		case "E":
+			windDirection = "east";
+			break;
+		case "SE":
+			windDirection = "south_east";
+			break;
+		case "S":
+			windDirection = "south";
+			break;
+		case "SW":
+			windDirection = "south_west";
+			break;
+		case "W":
+			windDirection = "west";
+			break;
+		case "NW":
+			windDirection = "north_west";
+			break;
+	}
+
 	return (
 		<>
 			<div
@@ -39,8 +68,19 @@ export default function Details({
 					<p>{`Min: ${data.min_temp} Max: ${data.max_temp}`}</p>
 					<p>{`Humidity: ${data.humidity}%`}</p>
 					<p>{`Certainty: ${data.predictability}%`}</p>
-					<p>{`Visibility: ${data.visibility}`}</p>
-					<p>{`Wind: ${data.wind_speed} ${data.wind_direction_compass}`}</p>
+					<p className="md-36">
+						<span className="material-icons mr-2 mtb-auto md-36">
+							visibility
+						</span>
+						{`${data.visibility}`}
+					</p>
+					<p className="md-36">
+						<span className="md-24">{`${data.wind_direction_compass}`}</span>
+						<span className="material-icons mr-2 mtb-auto md-36">
+							{windDirection}
+						</span>
+						{`${data.wind_speed}`}{" "}
+					</p>
 				</div>
 				<button
 					className={
