d109d101e7cd067e9e0a8d61bd63a750e9f619f0
Tobias Alpskog <tobbealp@hotmail.com>
2021-08-12 12:10:46 +0200

    added warning dummy data, warningTypes props to weather comp, removed some commented dummy data

--- src/App.js
+++ src/App.js
@@ -6,91 +6,93 @@ import "./App.css";
 import Weather from "./components/Weather";
 import Location from "./components/Location";
 
+const local_dummy_warnings = ["FIRE_RISK", "EXTREME_WARM"];
+
 const fadeIn = {
-	show: {
-		opacity: 1,
-		transition: {
-			duration: 0.5,
-		},
-	},
-	hidden: {
-		opacity: 0,
-	},
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
 };
 
 const item = {
-	show: (transitionDelay) => ({
-		opacity: 1,
-		transition: {
-			delay: 1 + transitionDelay,
-		},
-	}),
-	hidden: { opacity: 0 },
+  show: (transitionDelay) => ({
+    opacity: 1,
+    transition: {
+      delay: 1 + transitionDelay,
+    },
+  }),
+  hidden: { opacity: 0 },
 };
 
 const welcomeMessage =
-	"Please search for a city or use your current location to view the weather near you".split(
-		" "
-	);
+  "Please search for a city or use your current location to view the weather near you".split(
+    " "
+  );
 
 function computeDelay(delay, index, pauses) {
-	return delay * index + pauses * 1.4;
+  return delay * index + pauses * 1.4;
 }
 
 function addDelay(word, index, delay, pauseObject) {
-	let pauses = pauseObject.pauses;
+  let pauses = pauseObject.pauses;
 
-	let transitionDelay;
+  let transitionDelay;
 
-	if (word === "search") {
-		// Want same delay on search and location
-		pauseObject.inc();
-	} else if (word === "location") {
-		pauseObject.inc();
-	}
+  if (word === "search") {
+    // Want same delay on search and location
+    pauseObject.inc();
+  } else if (word === "location") {
+    pauseObject.inc();
+  }
 
-	transitionDelay = computeDelay(delay, index, pauses);
+  transitionDelay = computeDelay(delay, index, pauses);
 
-	return (
-		<motion.span
-			custom={transitionDelay}
-			variants={item}
-			initial="hidden"
-			animate="show"
-			key={`orchestration-span-${transitionDelay}`}
-		>
-			{`${word} `}
-		</motion.span>
-	);
+  return (
+    <motion.span
+      custom={transitionDelay}
+      variants={item}
+      initial="hidden"
+      animate="show"
+      key={`orchestration-span-${transitionDelay}`}
+    >
+      {`${word} `}
+    </motion.span>
+  );
 }
 
 function createSpanOrchestration(messageArray) {
-	let delay = 0.3;
+  let delay = 0.3;
 
-	const pauseObject = {
-		currentPauses: 0,
-		get pauses() {
-			return this.currentPauses;
-		},
-		inc() {
-			this.currentPauses++;
-		},
-	};
+  const pauseObject = {
+    currentPauses: 0,
+    get pauses() {
+      return this.currentPauses;
+    },
+    inc() {
+      this.currentPauses++;
+    },
+  };
 
-	const orchestration = messageArray.map((word, index) => {
-		return addDelay(word, index, delay, pauseObject);
-	});
+  const orchestration = messageArray.map((word, index) => {
+    return addDelay(word, index, delay, pauseObject);
+  });
 
-	return orchestration;
+  return orchestration;
 }
 
 async function getNearestCityByLocation(lat, long) {
-	const res = await fetch(
-		`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=${lat},${long}`
-	);
-	const data = await res.json();
+  const res = await fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?lattlong=${lat},${long}`
+  );
+  const data = await res.json();
 
-	/* Format: [
+  /* Format: [
 		{
 			distance: 0123,
 			title: "City name",
@@ -101,144 +103,102 @@ async function getNearestCityByLocation(lat, long) {
 	 ]
 	  */
 
-	const nearestData = data[0];
-	return nearestData;
+  const nearestData = data[0];
+  return nearestData;
 }
 
 function App() {
-	const [city, setCity] = useState("");
-	const [location, setLocation] = useState([0, 0]);
-	const [weatherDataToday, setWeatherDataToday] = useState({});
-	const [weeklyWeatherData, setWeeklyWeatherData] = useState([]);
+  const [city, setCity] = useState("");
+  const [location, setLocation] = useState([0, 0]);
+  const [weatherDataToday, setWeatherDataToday] = useState({});
+  const [weeklyWeatherData, setWeeklyWeatherData] = useState([]);
 
-	useEffect(() => {
-		if (location[0] !== 0 && location[1] !== 0) {
-			getWeatherData(location[0], location[1]);
-		}
-	}, [location]);
+  useEffect(() => {
+    if (location[0] !== 0 && location[1] !== 0) {
+      getWeatherData(location[0], location[1]);
+    }
+  }, [location]);
 
-	function updateLocation(latitude, longitude) {
-		setLocation([latitude, longitude]);
-	}
+  function updateLocation(latitude, longitude) {
+    setLocation([latitude, longitude]);
+  }
 
-	// const data1 = {
-	// 	id: 5078260196048896,
-	// 	weather_state_name: "Light Cloud",
-	// 	weather_state_abbr: "lc",
-	// 	wind_direction_compass: "NW",
-	// 	created: "2021-08-04T07:59:43.584220Z",
-	// 	applicable_date: "2021-08-04",
-	// 	min_temp: "12.3",
-	// 	max_temp: "20.4",
-	// 	the_temp: "19.7",
-	// 	wind_speed: "3.28",
-	// 	wind_direction: 309.1182428566674,
-	// 	air_pressure: 1015,
-	// 	humidity: 56,
-	// 	visibility: "12.4",
-	// 	predictability: 70,
-	// };
-	// const data2 = {
-	// 	id: 6079332784013312,
-	// 	weather_state_name: "Heavy Cloud",
-	// 	weather_state_abbr: "hc",
-	// 	wind_direction_compass: "SW",
-	// 	created: "2021-08-04T08:01:04.566080Z",
-	// 	applicable_date: "2021-08-04",
-	// 	min_temp: "10.8",
-	// 	max_temp: "21.1",
-	// 	the_temp: "19.9",
-	// 	wind_speed: "3.58",
-	// 	wind_direction: 215.7634508624013,
-	// 	air_pressure: 1014.5,
-	// 	humidity: 45,
-	// 	visibility: "11.8",
-	// 	predictability: 71,
-	// };
+  async function getWeatherData() {
+    // Use real location
+    const nearestCityInfo = await getNearestCityByLocation(
+      location[0],
+      location[1]
+    );
 
-	// function toggleDummyData() {
-	// 	if (city === "Copenhagen") {
-	// 		setWeatherDataToday(data2);
-	// 		setCity("Stockholm");
-	// 	} else {
-	// 		setWeatherDataToday(data1);
-	// 		setCity("Copenhagen");
-	// 	}
-	// }
+    // Update city..
 
-	async function getWeatherData() {
-		// Use real location
-		const nearestCityInfo = await getNearestCityByLocation(
-			location[0],
-			location[1]
-		);
+    const woeid = nearestCityInfo.woeid;
 
-		// Update city..
+    const weatherRes = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
+    );
 
-		const woeid = nearestCityInfo.woeid;
+    const data = await weatherRes.json();
 
-		const weatherRes = await fetch(
-			`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`
-		);
+    const weatherData = data.consolidated_weather[0];
 
-		const data = await weatherRes.json();
+    // Change to loop through properties. If they are number, set precision.
+    weatherData.min_temp = weatherData.min_temp.toPrecision(3);
+    weatherData.max_temp = weatherData.max_temp.toPrecision(3);
+    weatherData.the_temp = weatherData.the_temp.toPrecision(3);
+    weatherData.visibility = weatherData.visibility.toPrecision(3);
+    weatherData.wind_speed = weatherData.wind_speed.toPrecision(3);
 
-		const weatherData = data.consolidated_weather[0];
+    setWeatherDataToday(weatherData);
+    setCity(nearestCityInfo.title);
 
-		// Change to loop through properties. If they are number, set precision.
-		weatherData.min_temp = weatherData.min_temp.toPrecision(3);
-		weatherData.max_temp = weatherData.max_temp.toPrecision(3);
-		weatherData.the_temp = weatherData.the_temp.toPrecision(3);
-		weatherData.visibility = weatherData.visibility.toPrecision(3);
-		weatherData.wind_speed = weatherData.wind_speed.toPrecision(3);
+    const weeklyData = data.consolidated_weather.slice(1);
+    setWeeklyWeatherData(weeklyData);
+  }
 
-		setWeatherDataToday(weatherData);
-		setCity(nearestCityInfo.title);
-
-		const weeklyData = data.consolidated_weather.slice(1);
-		setWeeklyWeatherData(weeklyData);
-	}
-
-	// Should check if we have a city.
-	// If not, we display the welcome message.
-	return (
-		<div className="App">
-			<div className="outer-wrapper">
-				<header className="lg-fixed vfull">
-					<Location updateLocation={updateLocation} />
-				</header>
-				<main>
-					{/* <button onClick={toggleDummyData}>Toggle</button> */}
-					{city ? (
-						<div className="layout mh-center">
-							<Weather
-								city={city}
-								weatherDataToday={weatherDataToday}
-								weeklyWeatherData={weeklyWeatherData}
-							/>
-						</div>
-					) : (
-						<>
-							<div className="hfull flex column centered mh-auto">
-								<motion.h1
-									variants={fadeIn}
-									initial="hidden"
-									animate="show"
-									className="text centered p-2 italic"
-								>
-									Welcome!
-								</motion.h1>
-								<motion.div className="text centered p-4 italic">
-									{createSpanOrchestration(welcomeMessage)}
-								</motion.div>
-							</div>
-						</>
-					)}
-				</main>
-				{/* <Footer /> */}
-			</div>
-		</div>
-	);
+  // Should check if we have a city.
+  // If not, we display the welcome message.
+  return (
+    <div className="App">
+      <div className="outer-wrapper">
+        <header className="lg-fixed vfull">
+          <Location updateLocation={updateLocation} />
+        </header>
+        <main>
+          {/* <button onClick={toggleDummyData}>Toggle</button> */}
+          {city ? (
+            <>
+              <div className="layout mh-center">
+                <Weather
+                  city={city}
+                  weatherDataToday={weatherDataToday}
+                  weeklyWeatherData={weeklyWeatherData}
+                  warningTypes={local_dummy_warnings}
+                />
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="hfull flex column centered mh-auto">
+                <motion.h1
+                  variants={fadeIn}
+                  initial="hidden"
+                  animate="show"
+                  className="text centered p-2 italic"
+                >
+                  Welcome!
+                </motion.h1>
+                <motion.div className="text centered p-4 italic">
+                  {createSpanOrchestration(welcomeMessage)}
+                </motion.div>
+              </div>
+            </>
+          )}
+        </main>
+        {/* <Footer /> */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
